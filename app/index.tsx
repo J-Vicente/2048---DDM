b56c16b4011afe5a38194de71090c1dd4196b123
J-Vicente <joaovicent2713@gmail.com>
2025-07-28 10:58:55 -0300

    estrutura e arquivos criados

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,25 @@
-import { Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
 
 export default function Index() {
+  const router = useRouter();
+
+
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <TouchableOpacity>
+        <Text>Novo Jogo</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity>
+        <Text>Continuar</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity>
+        <Text>Ranking</Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
